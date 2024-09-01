14bb9388afcde7d08c4a38691640842931e7910b
Pav Munyphalla <lalaphalla@gmail.com>
2024-09-01 18:35:31 +0700

    chore: add mui-xchart

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,10 @@
+import SimpleBarChart from "../components/dashboard/SimpleBarChart";
 
 export default function DashboardPage() {
   const content = (
     <>
       <h1>Dashboard Page </h1>
+      <SimpleBarChart />
     </>
   );
   return content;
