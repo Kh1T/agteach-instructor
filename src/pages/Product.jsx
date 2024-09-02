fddc5f47c16eea6c8974fffc9be8578c68bec223
Pav Munyphalla <lalaphalla@gmail.com>
2024-09-02 16:40:26 +0700

    feat: add layout to product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,21 @@
-import { Box,  Typography } from "@mui/material";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 function ProductPage() {
   return (
     <Box sx={{ width: "100%" }}>
-      <Typography variant="h2">Product</Typography>
+      <Stack>
+        <Stack>
+          
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </Stack>
+      <Button
+        variant="contained"
+        sx={{ backgroundColor: "purple.main", textTransform: "uppercase" }}
+      >
+        Create Product
+      </Button>
+      </Stack>
+      
     </Box>
   );
 }
