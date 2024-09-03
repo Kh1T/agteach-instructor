ac43cf6a5492b07631c3ba2961e196e1777e086d
Pav Munyphalla <lalaphalla@gmail.com>
2024-09-03 10:46:02 +0700

    feat: add Custom table row feature

--- src/components/CustomTableMui.jsx
+++ src/components/CustomTableMui.jsx
@@ -4,33 +4,68 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { TablePagination } from "@mui/material";
+import { useState } from "react";
 
 export default function CustomTableMui({ data }) {
-    const {headers, rows} = data
+  const { headers, rows, isPagination = false } = data;
+
+  // Pagination state
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  // Handle page change
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  // Handle rows per page change
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0); // Reset to first page
+  };
+  const displayedRows = isPagination
+    ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+    : rows;
+
   const content = (
-    <TableContainer >
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {headers.map((title, id) => (
-              <TableCell key={id}>{title}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              // sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              {Object.values(row).map((cell, cellIndex)=>(
-                <TableCell key={cellIndex} >{cell}</TableCell>
+    <>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {headers.map((title, id) => (
+                <TableCell key={id}>{title}</TableCell>
               ))}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {displayedRows.map((row) => (
+              <TableRow
+                key={row.name}
+                // sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                {Object.values(row).map((cell, cellIndex) => (
+                  <TableCell key={cellIndex}>{cell}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {isPagination && (
+        <TablePagination
+          component="div"
+          count={rows.length}
+          page={page}
+          onPageChange={handleChangePage}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          rowsPerPageOptions={[5, 10, 25]}
+        />
+      )}
+    </>
   );
   return content;
 }
