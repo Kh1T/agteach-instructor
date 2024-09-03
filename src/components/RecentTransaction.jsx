ffae1a8e43f7f6b1ed9208b64f4feb245022aa75
Pav Munyphalla <lalaphalla@gmail.com>
2024-09-03 09:16:07 +0700

    refactor: delete uncessary code

--- src/components/RecentTransaction.jsx
+++ src/components/RecentTransaction.jsx
@@ -1,9 +1,3 @@
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import {
   Box,
@@ -12,7 +6,6 @@ import {
   MenuItem,
   Select,
   Stack,
-  Typography,
 } from "@mui/material";
 import CustomTableMui from "./CustomTableMui";
 import { useState } from "react";
@@ -52,15 +45,6 @@ function RecentTransaction() {
           title="Recent Transaction"
           content="Found(5) Items"
         />
-        {/* <Stack
-          direction="column"
-          sx={{
-            alignItems: "flex-start",
-          }}
-        >
-          <Typography variant="h6">Recent Transaction</Typography>
-          <Typography>Found(4) Items</Typography>
-        </Stack> */}
         <Box sx={{ minWidth: 180 }}>
           <FormControl fullWidth>
             <InputLabel id="demo-simple-select">Transaction</InputLabel>
@@ -77,34 +61,7 @@ function RecentTransaction() {
           </FormControl>
         </Box>
       </Stack>
-      <CustomTableMui data={data} />
-      {/* <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              {headers.map((title, id) => (
-                <TableCell key={id}>{title}</TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell>{row.calories}</TableCell>
-                <TableCell>{row.fat}</TableCell>
-                <TableCell>{row.carbs}</TableCell>
-                <TableCell>{row.protein}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer> */}
+      <CustomTableMui data={data} />{" "}
     </Paper>
   );
 }
