9ace6c225f67cc2d7ce530cfc9dac6a35d6f3230
Pav Munyphalla <lalaphalla@gmail.com>
2024-09-03 20:24:15 +0700

    feat: add purchase page

--- src/pages/Purchased.jsx
+++ src/pages/Purchased.jsx
@@ -1,9 +1,27 @@
+import {data} from "../data/sampleDashboardData";
+import { Box } from "@mui/material";
+import QueryHeader from "../components/QueryHeader";
+import { useState } from "react";
+import CustomTableMui from "../components/CustomTableMui";
 function PurchasedPage() {
-    return (
-        <>
-            <h1>Purhcased page</h1>
-        </>
-    )
+  const [selectState, setSelectState] = useState(0);
+  const label = "Filter";
+  function handleCreateNew() {}
+  function handleSearch() {}
+
+  return (
+    <Box>
+      <QueryHeader
+        label={label}
+        useSelectState={[selectState, setSelectState]}
+        selectData={["All", "Delivered", "Not Delivered"]}
+        handleCreateNew={handleCreateNew}
+        handleSearch={handleSearch}
+        isCreateNew = {false}
+      />
+      <CustomTableMui data={data} />
+    </Box>
+  );
 }
 
-export default PurchasedPage
+export default PurchasedPage;
