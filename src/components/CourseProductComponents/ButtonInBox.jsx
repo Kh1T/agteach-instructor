8cbc70a84381baafcbca26723c2d38df20131ffd
AnB School 0035 <anbschool0035@AnBs-Mac-mini-8.local>
2024-09-04 10:41:51 +0700

    feat: create function for add lecture for it to render and delete with order number logic.

--- src/components/CourseProductComponents/ButtonInBox.jsx
+++ src/components/CourseProductComponents/ButtonInBox.jsx
@@ -1,11 +1,22 @@
 import { Box, Button } from "@mui/material";
-export default function ButtonComponent({ text, variant, bgcolor, flexEnd, height, pl, startIcon  }) {
+export default function ButtonComponent({ text, variant, bgcolor, flexEnd, height, pl, startIcon, onClick  }) {
   return (
     <Box
-      sx={{ display: "flex", width: "100%",alignItems: "center", paddingY: 2 , pl:pl  }}
+      sx={{
+        display: "flex",
+        width: "100%",
+        alignItems: "center",
+        paddingY: 2,
+        pl: pl,
+      }}
       {...(flexEnd ? { justifyContent: "flex-end" } : null)}
     >
-      <Button variant={variant} sx={{  bgcolor:  bgcolor  , height: height}} startIcon={startIcon}>
+      <Button
+        variant={variant}
+        sx={{ bgcolor: bgcolor, height: height }}
+        startIcon={startIcon}
+        onClick={onClick}
+      >
         {text}
       </Button>
     </Box>
