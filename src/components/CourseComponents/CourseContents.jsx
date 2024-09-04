71c7b56449028f370b32828e7747a01d408850d4
AnB School 0035 <anbschool0035@AnBs-Mac-mini-8.local>
2024-09-04 11:41:20 +0700

    feat: create Section component and create function to able to populate when click add section and delete base on id.

--- src/components/CourseComponents/CourseContents.jsx
+++ src/components/CourseComponents/CourseContents.jsx
@@ -1,37 +1,32 @@
 import { Box, Typography, TextField, Divider, Stack } from "@mui/material";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-
 import ButtonComponent from "../CourseProductComponents/ButtonInBox";
 import IconWithTitle from "../CourseProductComponents/IconWithTitle";
 import BurstModeOutlinedIcon from "@mui/icons-material/BurstModeOutlined";
-import LectureComponent from "../CourseProductComponents/LectureComponent";
+import SectionComponent from "../CourseProductComponents/SectionComponent";
 
-import { v4 as uuidv4 } from "uuid"; // Import UUID
-
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 
 export default function CourseContents() {
-  // State to manage the list of lectures with unique IDs
-  let [lectures, setLectures] = useState([{ id: uuidv4(), number: 1 }]);
+  const [sections, setSection] = useState([{ id: uuidv4(), number: 1 }]);
 
-  const handleAddLecture = () => {
-    setLectures((prevLectures) => [
-      ...prevLectures.map((lecture, index) => ({
-        ...lecture,
+  const handleAddSection = () => {
+    setSection((prevSections) => [
+      ...prevSections.map((section, index) => ({
+        ...section,
         number: index + 1,
       })),
-      { id: uuidv4(), number: prevLectures.length + 1 }, // Add a new lecture with the next number
+      { id: uuidv4(), number: prevSections.length + 1 }, // Add a new lecture with the next number
     ]);
   };
 
-  const handleDeleteLecture = (id) => {
-    setLectures((prevLectures) =>
-      prevLectures
-        .filter((lecture) => lecture.id !== id)
-        .map((lecture, index) => ({ ...lecture, number: index + 1 }))
+  const handleDeleteSection = (id) => {
+    setSection((prevSections) =>
+      prevSections
+        .filter((section) => section.id !== id)
+        .map((section, index) => ({ ...section, number: index + 1 }))
     );
   };
-
   return (
     <Box my={2}>
       <Typography variant="h3"> </Typography>
@@ -40,39 +35,21 @@ export default function CourseContents() {
         icon={<BurstModeOutlinedIcon sx={{ color: "common.white" }} />}
       />
       <Divider sx={{ my: 2 }} />
-      <Box bgcolor={"grey.100"}>
-        <Stack direction={"row"} padding={2} justifyContent={"space-between"}>
-          <Typography variant="blgr">
-            <strong>Section 1:</strong> Write your section title below
-          </Typography>
-          <MoreVertIcon />
-        </Stack>
-        <TextField
-          sx={{ my: 2 }}
-          fullWidth
-          id="outlined-controlled"
-          label="eg: Introduction to indoor gardening"
+      {sections.map((section) => (
+        <SectionComponent
+          key={section.id}
+          id={section.id}
+          number={section.number}
+          onDelete={handleDeleteSection}
         />
-        <Box bgcolor="grey.300" padding={4} paddingTop={0}>
-          {lectures.map((lecture) => (
-            <LectureComponent
-              key={lecture.id}
-              id={lecture.id}
-              number={lecture.number}
-              onDelete={handleDeleteLecture}
-            />
-          ))}
-          <ButtonComponent
-            onClick={handleAddLecture}
-            text="Add Lecture +"
-            variant="outlined"
-            flexEnd
-            sx={{ px: 2 }}
-          />
-        </Box>
-        <Divider />
-        <ButtonComponent text={"Add Section +"} flexEnd variant={"contained"} />
-      </Box>
+      ))}
+      <Divider />
+      <ButtonComponent
+        onClick={handleAddSection}
+        text={"Add Section +"}
+        flexEnd
+        variant={"contained"}
+      />
     </Box>
   );
 }
---
+++ src/components/CourseProductComponents/SectionComponent.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { Box, Stack, Typography, TextField } from "@mui/material";
+import LectureComponent from "./LectureComponent"; // Import your LectureComponent
+import ButtonComponent from "./ButtonInBox";
+import { v4 as uuidv4 } from "uuid";
+import { Delete } from "@mui/icons-material";
+import { MoreVertRounded } from "@mui/icons-material";
+
+export default function SectionComponent({id, onDelete , number }) {
+  const [lectures, setLectures] = useState([{ id: uuidv4(), number: 1 }]);
+
+  const handleAddLecture = () => {
+    setLectures((prevLectures) => [
+      ...prevLectures.map((lecture, index) => ({
+        ...lecture,
+        number: index + 1,
+      })),
+      { id: uuidv4(), number: prevLectures.length + 1 }, // Add a new lecture with the next number
+    ]);
+  };
+
+  const handleDeleteLecture = (id) => {
+    setLectures((prevLectures) =>
+      prevLectures
+        .filter((lecture) => lecture.id !== id)
+        .map((lecture, index) => ({ ...lecture, number: index + 1 }))
+    );
+  };
+
+  const [showDelete, setShowDelete] = useState(false);
+
+  const handleClickOnVert = () => {
+    setShowDelete((prevShowDelete) => !prevShowDelete);
+  };
+
+  return (
+    <Box bgcolor="grey.100" padding={2} mb={2}>
+      <Stack
+        position="relative"
+        direction="row"
+        padding={2}
+        justifyContent="space-between"
+      >
+        <Typography variant="blgr">
+          <strong>Section {number}:</strong> Write your section title below
+        </Typography>
+        <MoreVertRounded onClick={handleClickOnVert} />
+        {showDelete && (
+          <Stack
+            onClick={() => onDelete(id)} // Pass a function reference
+            direction={"row"}
+            boxShadow={"0px 2px 4px rgba(0, 0, 0, 0.15)"}
+            sx={{
+              zIndex: 1,
+              position: "absolute",
+              top: 40,
+              right: 20,
+              bgcolor: "common.white",
+              p: 1,
+              gap: 1,
+              justifyItems: "center",
+            }}
+          >
+            <Delete color="red"  />
+            <Typography color="red">Delete</Typography>
+          </Stack>
+        )}
+      </Stack>
+      <TextField
+        sx={{ my: 2 }}
+        fullWidth
+        id="outlined-controlled"
+        label="eg: Introduction to indoor gardening"
+      />
+      <Box bgcolor="grey.300" padding={4} paddingTop={0}>
+        {lectures.map((lecture) => (
+          <LectureComponent
+            key={lecture.id}
+            id={lecture.id}
+            number={lecture.number}
+            onDelete={handleDeleteLecture}
+          />
+        ))}
+        <ButtonComponent
+          onClick={handleAddLecture}
+          text="Add Lecture +"
+          variant="outlined"
+          flexEnd
+          sx={{ px: 2 }}
+        />
+      </Box>
+    </Box>
+  );
+}
