1817ec4d0baff752eb0c81b698f34abc312d276c
Pav Munyphalla <lalaphalla@gmail.com>
2024-09-05 18:52:15 +0700

    refactor: change data to folder data

--- src/components/RecentTransaction.jsx
+++ src/components/RecentTransaction.jsx
@@ -1,95 +1,60 @@
- 
 import {
-    Box,
-    FormControl,
-    InputLabel,
-    MenuItem,
-    Select,
-    Stack,
-  } from "@mui/material"; 
-  import { useState } from "react";
-  import CustomTableHeader from "./CustomTableHeader";
-  import CustomTable from "./CustomTable";
-  
-  
-  function RecentTransaction() {
- 
-    const [transaction, setTransaction] = useState();
-  const data = [
-    {
-      no: 1,
-      name: "Sok",
-      date : '20-12-2020',
-      amount : 10,
-    },
-    {
-      no: 2,
-      name: "Sok",
-      date : '20-12-2020',
-      amount : 10,
-    },
-    {
-      no: 3,
-      name: "Sok",
-      date : '20-12-2020',
-      amount : 10,
-    },
-    {
-      no: 4,
-      name: "Sok",
-      date : '20-12-2020',
-      amount : 10,
-    },
-    {
-      no: 5,
-      name: "Sok",
-      date : '20-12-2020',
-      amount : 10,
-    },
-    ]
-    return (
-      <Box
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+} from "@mui/material";
+import { useState } from "react";
+import CustomTableHeader from "./CustomTableHeader";
+import CustomTable from "./CustomTable";
+import { recentData } from "../data/recentData";
+
+function RecentTransaction() {
+  const [transaction, setTransaction] = useState();
+  return (
+    <Box
+      sx={{
+        height: "440px",
+        px: "20px",
+        borderRadius: 4,
+        boxShadow: "0px 10px 33px 0px rgba(5,27,58,0.1)",
+      }}
+    >
+      <Stack
+        direction="row"
         sx={{
-          height: "440px",
-          px: "20px",
-          borderRadius: 4,
-          boxShadow: "0px 10px 33px 0px rgba(5,27,58,0.1)",
+          pt: "30px",
+          pb: "15px",
+          justifyContent: "space-between",
+          alignItems: "flex-start",
         }}
       >
-        <Stack
-          direction="row"
-          sx={{
-            pt: "30px",
-            pb: "15px",
-            justifyContent: "space-between",
-            alignItems: "flex-start",
-          }}
-        >
-          <CustomTableHeader
-            title="Recent Transaction"
-            content="Found(5) Items"
-          />
-          <Box sx={{ minWidth: 180 }}>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select">Transaction</InputLabel>
-              <Select
-                id="demo-simple-select"
-                value={transaction}
-                onChange={(e) => setTransaction(e.target.value)}
-                label="Transaction"
-                defaultValue="10"
-              >
-                <MenuItem value={10}>Course</MenuItem>
-                <MenuItem value={20}>Product</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-        </Stack>
-        
-        <CustomTable data={data} />
-      </Box>
-    ); 
-  }
-  
-  export default RecentTransaction;
-  
+        <CustomTableHeader
+          title="Recent Transaction"
+          content="Found(5) Items"
+        />
+        <Box sx={{ minWidth: 180 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select">Transaction</InputLabel>
+            <Select
+              id="demo-simple-select"
+              value={transaction}
+              onChange={(e) => setTransaction(e.target.value)}
+              label="Transaction"
+              defaultValue="10"
+            >
+              <MenuItem value={10}>Course</MenuItem>
+              <MenuItem value={20}>Product</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+      </Stack>
+
+      <CustomTable data={recentData} />
+    </Box>
+  );
+}
+
+export default RecentTransaction;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Box,  Stack,  } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import TopPerformance from "../components/dashboard/TopPerformance";
 import RecentTransaction from "../components/RecentTransaction";
 import Grid from "@mui/material/Grid2";
@@ -12,21 +12,21 @@ export default function DashboardPage() {
     <>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={4}>
-          <Grid size={{xs: 12, md: 8}}>
+          <Grid size={{ xs: 12, md: 8 }}>
             <Overview />
           </Grid>
-          <Grid size={{xs: 12, md: 4}}>
+          <Grid size={{ xs: 12, md: 4 }}>
             <Box>
               <RecentTransaction />
             </Box>
           </Grid>
-          <Grid size={{xs: 12, md: 4}}>
+          <Grid size={{ xs: 12, md: 4 }}>
             <Stack direction="column" spacing={2}>
               <CardSale />
               <CardSale />
             </Stack>
           </Grid>
-          <Grid size={{xs: 12, md: 8}}>
+          <Grid size={{ xs: 12, md: 8 }}>
             <TopPerformance />
           </Grid>
         </Grid>
