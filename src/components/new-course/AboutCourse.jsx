64f529c729a94bf5ef4861f75d15a4db1e43b9a7
sovanchitra <chivsovanchetra@gmail.com>
2024-09-26 14:28:21 +0700

    feat-add-course: retrieve data

--- src/components/new-course/AboutCourse.jsx
+++ src/components/new-course/AboutCourse.jsx
@@ -12,7 +12,11 @@ import TextSection from "../course-product/TextSection";
  * @returns a JSX element containing the AboutCourse component.
  */
 
-export default function AboutCourse() {
+export default function AboutCourse({ register, errors, setValue }) {
+   const handleChange = () => {
+    setValue("description", setValue("description"));
+   }
+
   return (
     <Box className="container">
       <IconWithTitle
@@ -21,7 +25,7 @@ export default function AboutCourse() {
       />
       <Divider sx={{ my: 2 }} />
       <TextSection
-        title="Let’s craft a course title"
+        title="Let's craft a course title"
         description="Crafted a good title would help your content engage more students."
       />
       <TextField
@@ -29,6 +33,10 @@ export default function AboutCourse() {
         fullWidth
         id="outlined-controlled"
         label="Enter your course title"
+        {...register("title", { required: "Title is required" })}
+        error={!!errors.title}
+        helperText={errors.title?.message}
+        defaultValue={setValue}
       />
       <Typography variant="bsr" color="dark.300" sx={{ mt: 2 }}>
         eg: How to plant an indoor tomatoes 100% edible
@@ -41,11 +49,16 @@ export default function AboutCourse() {
         />
         <TextField
           slotProps={{
-            input: { sx: { alignItems: "flex-start", height: "156px" } },
+            input: { sx: { alignItems: "flex-start", minHeight: "150px" } },
           }}
           sx={{ my: 2 }}
           fullWidth
+          multiline
           label="Enter your course description"
+          {...register("description", { required: "Description is required" })}
+          error={!!errors.description}
+          helperText={errors.description?.message}
+          defaultValue={setValue("description")}
         />
       </Box>
       <TextSection
@@ -55,12 +68,16 @@ export default function AboutCourse() {
       />
       <TextField
         slotProps={{
-          input: { sx: { alignItems: "flex-start", height: "156px" } },
+          input: { sx: { alignItems: "flex-start", minHeight: "150px" } },
         }}
         sx={{ my: 2 }}
         fullWidth
+        multiline
         id="outlined-controlled"
-        label="What  they will learn in this course: "
+        label="What they will learn in this course: "
+        {...register("objective", { required: "Objective is required" })} 
+        error={!!errors.objective}
+        helperText={errors.objective?.message}
       />
     </Box>
   );
