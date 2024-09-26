38479a2ec1599cc4d3d99acf255d4fd8d6b5d950
sovanchitra <chivsovanchetra@gmail.com>
2024-09-26 14:28:21 +0700

    feat-add-course: retrieve data

--- src/pages/NewCourse.jsx
+++ src/pages/NewCourse.jsx
@@ -8,29 +8,43 @@ import ButtonComponent from "../components/course-product/ButtonInBox";
 
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import { useNavigate } from "react-router";
+import { useForm } from "react-hook-form";
 
 function NewCoursePage() {
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
+  const submitHandler = (data) => {
+    console.log(data);
+  }
+
   return (
     <Box sx={{ width: "100%", paddingBottom: "200px" }}>
-      <Button
-        variant="Text"
-        startIcon={<ArrowBackIosIcon fontSize="small" color="dark.300" />}
-        sx={{ textDecoration: "underline", color: "dark.300" }}
-        onClick={() => navigate(-1)}
-      >
-        <Typography variant="bsr">Go Back</Typography>
-      </Button>
-      <AboutCourse />
-      <CourseContents />
-      <CoursePrice />
-      <AddThumbnail />
-      <RelatedProduct />
-      <ButtonComponent
-        text={"CREATE COURSE"}
-        variant={"contained"}
-        bgcolor={"purple.main"}
-      />
+      <form onSubmit={handleSubmit(submitHandler)} >
+        <Button
+          variant="Text"
+          startIcon={<ArrowBackIosIcon fontSize="small" color="dark.300" />}
+          sx={{ textDecoration: "underline", color: "dark.300" }}
+          onClick={() => navigate(-1)}
+        >
+          <Typography variant="bsr">Go Back</Typography>
+        </Button>
+        <AboutCourse register={register} errors={errors} setValue={setValue} />
+        {/* <CourseContents /> */}
+        <CoursePrice />
+        {/* <AddThumbnail /> */}
+        <RelatedProduct />
+        <ButtonComponent
+          text={"CREATE COURSE"}
+          variant={"contained"}
+          bgcolor={"purple.main"}
+        />
+      </form>
     </Box>
   );
 }
