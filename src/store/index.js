40fab359151cb5fe47241becd423a693f8b2f12e
sokhaneadh <sokhaneadh@gmail.com>
2024-09-27 15:47:44 +0700

    feat-course-page: create new api file, fixing the delete api

--- src/store/index.js
+++ src/store/index.js
@@ -3,18 +3,21 @@ import { authApi } from "../services/api/authApi";
 import { locationApi } from "../services/api/locationApi";
 import { productApi } from "../services/api/productApi";
 import userReducer from "../features/user/userSlice";
+import { courseApi } from "../services/api/courseApi";
 
 export const store = createStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
     [locationApi.reducerPath]: locationApi.reducer,
     [productApi.reducerPath]: productApi.reducer,
+    [courseApi.reducerPath]: courseApi.reducer,
     user: userReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       authApi.middleware,
       locationApi.middleware,
-      productApi.middleware
+      productApi.middleware,
+      courseApi.middleware
     ),
 });
