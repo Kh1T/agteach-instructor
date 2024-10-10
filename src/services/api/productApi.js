464f1e857b251c6e67c11319776a009c7a15195f
Pav Munyphalla <lalaphalla@gmail.com>
2024-10-10 18:40:39 +0700

    fix: update api to agteach

--- src/services/api/productApi.js
+++ src/services/api/productApi.js
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productApi = createApi({
   reducerPath: "productApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: " http://localhost:3001",
+    baseUrl: "https://api.agteach.site",
     credentials: "include",
   }),
   tagTypes: ["Product"],
