e57c0797ad9976f666b3109fdb5c00726288eeca
Pann Senglay <senglaypann@gmail.com>
2024-11-12 14:52:08 +0700

    feat: added pattern validation to firstname in setting page

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -269,6 +269,10 @@ function SettingPage() {
                   fullWidth
                   {...registerBasicInfo("firstName", {
                     required: "First name is required",
+                    pattern: {
+                      value: /^[A-Za-z]+$/i,
+                      message: "First name can only contain letters",
+                    },
                     validate: (value) => {
                       if (value.length < 2) return "First name must be at least 2 characters";
                       if (value.length > 25) return "First name must be at most 25 characters";
