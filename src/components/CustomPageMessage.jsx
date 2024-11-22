962af3440b50cd6a8bf1eeea5b08e44d2fe2a019
sela-bet <betsela555@gmail.com>
2024-11-22 10:16:41 +0700

    feat: add image param to CustomPageMessage

--- src/components/CustomPageMessage.jsx
+++ src/components/CustomPageMessage.jsx
@@ -1,24 +1,25 @@
-import { Box, Stack, Typography } from "@mui/material";
-import IconNotApprove from '../assets/dashboard-not-approve.svg';
+import { Box, Stack, Typography } from '@mui/material';
 
-export const CustomPageMessage = ({title, subtitle, }) => {
-    return       <Stack component="section" sx={{ backgroundColor: 'grey.100', pb: 5 }}>
-    <Stack
-      direction="column"
-      sx={{
-        alignContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Box component="img" src={IconNotApprove} />
-      <Stack spacing={2} sx={{ textAlign: 'center', width: '360px' }}>
-        <Typography variant="h4" color="primary">
-          {title}
-        </Typography>
-        <Typography variant="bmdr" color="dark.200">
-          {subtitle}
-        </Typography>
+export const CustomPageMessage = ({ title, subtitle, image }) => {
+  return (
+    <Stack component="section" sx={{ backgroundColor: 'grey.100', pb: 5 }}>
+      <Stack
+        direction="column"
+        sx={{
+          alignContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Box component="img" src={image} />
+        <Stack spacing={2} sx={{ textAlign: 'center', width: '360px' }}>
+          <Typography variant="h4" color="primary">
+            {title}
+          </Typography>
+          <Typography variant="bmdr" color="dark.200">
+            {subtitle}
+          </Typography>
+        </Stack>
       </Stack>
     </Stack>
-  </Stack>;
+  );
 };
