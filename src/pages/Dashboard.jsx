0ecba7cd5f17f6c4d8592342a46d2ca5185db742
sela-bet <betsela555@gmail.com>
2024-11-22 14:12:24 +0700

    style: remove unused component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,13 @@
-import { Box } from "@mui/material";
-import TopPerformance from "../components/dashboard/TopPerformance";
 import Grid from "@mui/material/Grid2";
+
+import TopPerformance from "../components/dashboard/TopPerformance";
 import Overview from "../components/dashboard/Overview";
 import NotApprove from "../components/dashboard/NotApprove";
+
 export default function DashboardPage() {
   const isApprove = false;
   const notApprovedContent = <NotApprove />;
   const approvedContent = (
-    <Box sx={{ flexGrow: 1, mb: 4 }}>
       <Grid container spacing={4}>
         <Grid size={{ xs: 12, md: 12 }}>
           <Overview />
@@ -16,7 +16,6 @@ export default function DashboardPage() {
           <TopPerformance />
         </Grid>
       </Grid>
-    </Box>
   );
   const content = isApprove ? approvedContent : notApprovedContent;
 
