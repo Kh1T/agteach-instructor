1fac66eb1a7497c32d816f3cce06c80ca8f67439
sela-bet <betsela555@gmail.com>
2024-11-22 10:24:26 +0700

    chore: remove unused component

--- src/components/CustomPageMessage.jsx
+++ src/components/CustomPageMessage.jsx
@@ -2,23 +2,19 @@ import { Box, Stack, Typography } from '@mui/material';
 
 export const CustomPageMessage = ({ title, subtitle, image }) => {
   return (
-    <Stack component="section" sx={{ backgroundColor: 'grey.100', pb: 5 }}>
-      <Stack
-        direction="column"
-        sx={{
-          alignContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Box component="img" src={image} />
-        <Stack spacing={2} sx={{ textAlign: 'center', width: '360px' }}>
-          <Typography variant="h4" color="primary">
-            {title}
-          </Typography>
-          <Typography variant="bmdr" color="dark.200">
-            {subtitle}
-          </Typography>
-        </Stack>
+    <Stack
+      sx={{ backgroundColor: 'grey.100', pb: 5 }}
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Box component="img" src={image} width={250} />
+      <Stack spacing={2} sx={{ textAlign: 'center', width: '360px' }}>
+        <Typography variant="h4" color="primary">
+          {title}
+        </Typography>
+        <Typography variant="bsr" color="dark.200">
+          {subtitle}
+        </Typography>
       </Stack>
     </Stack>
   );
