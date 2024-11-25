016f618b46405174ba66aa53853243313a315511
sovanchitra <chivsovanchetra@gmail.com>
2024-11-25 17:34:15 +0700

    feat: restrict user based on approval status

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,36 +1,53 @@
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { CircularProgress, Container } from '@mui/material';
+import { useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { CircularProgress, Container } from "@mui/material";
 
 export default function ProtectedRoute({ children }) {
   const { isAuthenticated, isVerify, isLoading } = useSelector(
     (state) => state.auth
   );
+  const { isApproved } = useSelector((state) => state.approval);
   const navigate = useNavigate();
+
+  const location = useLocation();
+  console.log(location);
   useEffect(() => {
     if (isAuthenticated && !isVerify) {
-      navigate('/auth/signup/verification');
+      navigate("/auth/signup/verification");
     }
-    if (!isLoading && !isAuthenticated &&!isVerify) {
-      navigate('/auth/login');
+    if (!isLoading && !isAuthenticated && !isVerify) {
+      navigate("/auth/login");
     }
-  }, [isAuthenticated, isLoading, navigate]);
+    if (location.pathname === "/setting") {
+      return () => children;
+    }
+    if (isAuthenticated && isVerify && !isApproved) {
+      navigate("/");
+    }
+  }, [
+    isAuthenticated,
+    isLoading,
+    isVerify,
+    isApproved,
+    location,
+    children,
+    navigate,
+  ]);
 
   if (isLoading) {
     return (
       <Container
         sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
         }}
       >
         <CircularProgress />
       </Container>
     );
   }
-
   return children;
 }
